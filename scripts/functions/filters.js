eab0e351b06326ed476fb17beb5b25366f2862ee
Kushchina Alena <alenakushchina@gmail.com>
2024-10-03 21:47:17 +0200

    Switched to algorithm using cycles instead of methods

--- scripts/functions/filters.js
+++ scripts/functions/filters.js
@@ -4,22 +4,120 @@ import { selectedFilters } from "../search.js";
 
 // Filter recipes by text query
 export function filterRecipes(query, allRecipes) {
-    return allRecipes.filter(recipe => {
+    const filteredRecipes = [];
+
+    for (let i = 0; i < allRecipes.length; i++) {
+        const recipe = allRecipes[i];
         const recipeName = recipe.name.toLowerCase();
         const recipeDescription = recipe.description.toLowerCase();
-        let matches = recipeName.includes(query) || recipeDescription.includes(query);
-
-        if (!matches) matches = recipe.ingredients.some(ingredientObj => ingredientObj.ingredient.toLowerCase().includes(query));
-        return matches;
-    });
+        
+        let matches = false;
+    
+        // Check if `query` is contained in `recipeName`
+        for (let i = 0; matches !== true && i <= recipeName.length - query.length; i++) {
+            if (recipeName.substr(i, query.length) === query) {
+                matches = true;
+            }
+        }
+    
+        // If `recipeName` does not contain `query`, check `recipeDescription`
+        if (!matches) {
+            for (let i = 0; matches !== true && i <= recipeDescription.length - query.length; i++) {
+                if (recipeDescription.substr(i, query.length) === query) {
+                    matches = true;
+                }
+            }
+        }
+    
+        // If there are no matches in the name and description, we check the ingredients
+        if (!matches) {
+            for (let j = 0; matches !== true && j < recipe.ingredients.length; j++) {
+                const ingredient = recipe.ingredients[j].ingredient.toLowerCase();
+                for (let k = 0; matches !== true && k <= ingredient.length - query.length; k++) {
+                    if (ingredient.substr(k, query.length) === query) {
+                        matches = true;
+                    }
+                }
+            }
+        }
+    
+        // Add filtered recipes
+        if (matches) {
+            filteredRecipes[filteredRecipes.length] = recipe;
+        }
+    }
+    
+    return filteredRecipes;
 }
 
 // Filter recipes by active tags
 export function filterByAdditionalFilters(filteredRecipes) {
-    return filteredRecipes.filter(recipe => {
-        const ingredientMatch = selectedFilters.ingredients.length === 0 || selectedFilters.ingredients.every(filter => recipe.ingredients.some(ingredientObj => ingredientObj.ingredient.toLowerCase() === filter));
-        const applianceMatch = selectedFilters.appliance.length === 0 || selectedFilters.appliance.includes(recipe.appliance.toLowerCase());
-        const ustensilMatch = selectedFilters.ustensils.length === 0 || selectedFilters.ustensils.every(filterUstensil => recipe.ustensils.some(ustensil => ustensil.toLowerCase() === filterUstensil));
-        return ingredientMatch && applianceMatch && ustensilMatch;
-    });
+    const finalFilteredRecipes = [];  // Create an array to store matching recipes
+
+    for (let i = 0; i < filteredRecipes.length; i++) {
+        const recipe = filteredRecipes[i];
+
+        // Check ingredients
+        let ingredientMatch = selectedFilters.ingredients.length === 0;
+        if (!ingredientMatch) {
+            for (let j = 0; j < selectedFilters.ingredients.length; j++) {
+                let ingredientFound = false;
+                const filter = selectedFilters.ingredients[j];
+
+                // Traverse all ingredients in the recipe
+                for (let k = 0; k < recipe.ingredients.length && !ingredientFound; k++) {
+                    if (recipe.ingredients[k].ingredient.toLowerCase() === filter) {
+                        ingredientFound = true;
+                    }
+                }
+
+                // If one of the filters is not found, the ingredientMatch should be false
+                ingredientMatch = ingredientFound;
+                if (!ingredientMatch) {
+                    j = selectedFilters.ingredients.length;  // Stop further iterations in outer loop
+                }
+            }
+        }
+
+        // Check appliances
+        let applianceMatch = selectedFilters.appliance.length === 0;
+        if (!applianceMatch) {
+            for (let j = 0; j < selectedFilters.appliance.length && !applianceMatch; j++) {
+                if (recipe.appliance.toLowerCase() === selectedFilters.appliance[j]) {
+                    applianceMatch = true;
+                }
+            }
+        }
+
+        // Check utensils
+        let ustensilMatch = selectedFilters.ustensils.length === 0;
+        if (!ustensilMatch) {
+            for (let j = 0; j < selectedFilters.ustensils.length; j++) {
+                let ustensilFound = false;
+                const filterUstensil = selectedFilters.ustensils[j];
+
+                // Traverse all utensils in the recipe
+                for (let k = 0; k < recipe.ustensils.length && !ustensilFound; k++) {
+                    if (recipe.ustensils[k].toLowerCase() === filterUstensil) {
+                        ustensilFound = true;
+                    }
+                }
+
+                // If one of the filters is not found, the ustensilMatch should be false
+                ustensilMatch = ustensilFound;
+                if (!ustensilMatch) {
+                    j = selectedFilters.ustensils.length;  // Stop further iterations in outer loop
+                }
+            }
+        }
+
+        // If all conditions are met, add the recipe to the final array
+        if (ingredientMatch && applianceMatch && ustensilMatch) {
+            finalFilteredRecipes.push(recipe);
+        }
+    }
+
+    return finalFilteredRecipes;  // Return the filtered recipes
 }
+
+
--- scripts/search.js
+++ scripts/search.js
@@ -60,29 +60,7 @@ function updateDropdowns(filteredRecipes) {
     createDropdownList(getRecipeData(filteredRecipes, recipe => recipe.appliance), 'appliance-list', 'appliance-btn', 'appliance-dropdown', 'appliance-arrow');
     createDropdownList(getRecipeData(filteredRecipes, recipe => recipe.ustensils), 'ustensils-list', 'ustensils-btn', 'ustensils-dropdown', 'ustensils-arrow');
 }
-/*
-// Filter recipes by text query
-function filterRecipes(query, allRecipes) {
-    return allRecipes.filter(recipe => {
-        const recipeName = recipe.name.toLowerCase();
-        const recipeDescription = recipe.description.toLowerCase();
-        let matches = recipeName.includes(query) || recipeDescription.includes(query);
 
-        if (!matches) matches = recipe.ingredients.some(ingredientObj => ingredientObj.ingredient.toLowerCase().includes(query));
-        return matches;
-    });
-}
-
-// Filter recipes by active tags
-function filterByAdditionalFilters(filteredRecipes) {
-    return filteredRecipes.filter(recipe => {
-        const ingredientMatch = selectedFilters.ingredients.length === 0 || selectedFilters.ingredients.every(filter => recipe.ingredients.some(ingredientObj => ingredientObj.ingredient.toLowerCase() === filter));
-        const applianceMatch = selectedFilters.appliance.length === 0 || selectedFilters.appliance.includes(recipe.appliance.toLowerCase());
-        const ustensilMatch = selectedFilters.ustensils.length === 0 || selectedFilters.ustensils.every(filterUstensil => recipe.ustensils.some(ustensil => ustensil.toLowerCase() === filterUstensil));
-        return ingredientMatch && applianceMatch && ustensilMatch;
-    });
-}
-*/
 // Update selected filters
 function updateSelectedFilters(field, filterValue) {
     if (!selectedFilters[field].includes(filterValue)) selectedFilters[field].push(filterValue);
